28bb17772794e098cc9a56e98cba541507ebd3a4
Tomas Morstein <tmr@idea.cz>
2014-04-16 17:23:52 +0200

    add an example to show service_broker usage

---
+++ apps/dws/src/dws_service_example.erl
@@ -0,0 +1,42 @@
+-module (dws_service_example).
+-export ([
+          echo/2,
+          get_system_processes/1,
+          get_mnesia_info/1
+         ]).
+
+echo (_SessionID, Msg) -> {ok, Msg}.
+
+get_system_processes (_SessionID) ->
+    {ok, {array, [
+                  {struct, [{pid_to_list (Pid), get_process_ancestors (Pid)}]}
+                  || Pid <- erlang:processes ()
+                 ]}}.
+
+get_mnesia_info (_SessionID) ->
+    A = fun (X) -> {array, X} end,  %% Array
+    S = fun (X) -> X end,           %% Scalar
+    Props = [
+             {extra_db_nodes, A},
+             {db_nodes, A},
+             {running_db_nodes, A},
+             {is_running, S},
+             {local_tables, A},
+             {tables, A}
+            ],
+    Info = [ {K, F (mnesia:system_info (K))}
+             || {K, F} <- Props ],
+    {ok, {struct, Info}}.
+
+%% === Private functions ===
+
+proc_to_list (undefined) -> null;
+proc_to_list (Pid) when is_pid (Pid) -> pid_to_list (Pid);
+proc_to_list (Name) when is_atom (Name) -> atom_to_list (Name).
+
+get_process_ancestors (Pid) ->
+    ProcInfo = erlang:process_info (Pid),
+    Dict = proplists:get_value (dictionary, ProcInfo, []),
+    Ancestors = proplists:get_value ('$ancestors', Dict, []),
+    {array, [ proc_to_list (A) || A <- Ancestors ]}.
+
